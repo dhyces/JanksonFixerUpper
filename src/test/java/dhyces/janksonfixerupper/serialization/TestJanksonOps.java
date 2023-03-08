ed78281280c499d554e89aa946e799bf8b93a518
dhyces <pokmon987@gmail.com>
2023-03-08 18:38:53 -0500

    Update TestJanksonOps.java

--- src/test/java/dhyces/janksonfixerupper/serialization/TestJanksonOps.java
+++ src/test/java/dhyces/janksonfixerupper/serialization/TestJanksonOps.java
@@ -1,8 +1,6 @@
 package dhyces.janksonfixerupper.serialization;
 
-import blue.endless.jankson.Jankson;
-import blue.endless.jankson.JsonElement;
-import blue.endless.jankson.JsonGrammar;
+import blue.endless.jankson.*;
 import blue.endless.jankson.api.SyntaxError;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -10,6 +8,8 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.JsonOps;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import dhyces.janksonfixerupper.serialization.comments.Comment;
+import dhyces.janksonfixerupper.serialization.io.JanksonIoHelper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.CleanupMode;
 import org.junit.jupiter.api.io.TempDir;
@@ -24,6 +24,7 @@ import java.util.List;
 
 public class TestJanksonOps {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().setLenient().create();
+    private static final Jankson JANKSON = Jankson.builder().build();
     private static final JsonGrammar GRAMMER = new JsonGrammar.Builder().bareSpecialNumerics(true).printUnquotedKeys(true).build();
     private static final Logger LOGGER = LoggerFactory.getLogger(TestJanksonOps.class);
 
@@ -31,8 +32,6 @@ public class TestJanksonOps {
 
     @Test
     public void write(@TempDir(cleanup = CleanupMode.NEVER) Path path) {
-        System.out.println(System.getenv());
-        System.out.println(System.getProperties());
         Path jankFile = path.resolve("jank_test.json5");
         TestObject obj = new TestObject("hello", 4.5, List.of(4.1f, 2.7f, 5.23423f));
         DataResult<JsonElement> test = TestObject.CODEC.encodeStart(JanksonOps.INSTANCE, obj);
@@ -47,26 +46,33 @@ public class TestJanksonOps {
     }
 
     @Test
+    public void writeRegularJank(@TempDir(cleanup = CleanupMode.NEVER) Path path) throws IOException {
+        Path jank = path.resolve("test.json5");
+        JsonObject json = new JsonObject();
+        json.put("someStr", JsonPrimitive.of("hello"));
+        json.put("someDouble", JsonPrimitive.of(4.5));
+        // Regular jankson has precision error here
+        json.put("floatList", new JsonArray(List.of(4.2f, 3.1f), JANKSON.getMarshaller()));
+        Files.write(jank, json.toJson(GRAMMER).getBytes());
+    }
+
+    @Test
     public void read() {
         Path readTest = TEST_RESOURCES.resolve("jankson_read.json5");
-        try {
-            DataResult<TestObject> test = TestObject.CODEC.parse(JanksonOps.INSTANCE, Jankson.builder().build().load(readTest.toFile()));
-            if (test.error().isPresent()) {
-                LOGGER.error(() -> test.error().get().message());
-            }
-            TestObject o = test.result().get();
-            int i = 0;
-        } catch (IOException | SyntaxError e) {
-            throw new RuntimeException(e);
+        DataResult<TestObject> test = JanksonIoHelper.parseCodec(readTest, TestObject.CODEC);
+        if (test.error().isPresent()) {
+            LOGGER.error(() -> test.error().get().message());
+            return;
         }
+        TestObject o = test.result().get();
     }
 
     private record TestObject(String someStr, double someDouble, List<Float> floatList) {
         public static final Codec<TestObject> CODEC = RecordCodecBuilder.create(instance ->
                 instance.group(
-                        Codec.STRING.fieldOf("someStr").forGetter(TestObject::someStr),
-                        Codec.DOUBLE.fieldOf("someDouble").forGetter(TestObject::someDouble),
-                        CommentedCodec.commented(Codec.list(Codec.FLOAT), "Comment").fieldOf("floatList").forGetter(TestObject::floatList)
+                        CommentedCodec.commented(Codec.STRING, "This is a comment for the string object someStr").fieldOf("someStr").forGetter(TestObject::someStr),
+                        CommentedCodec.commented(Codec.DOUBLE, "This is a comment on the double!").fieldOf("someDouble").forGetter(TestObject::someDouble),
+                        CommentedCodec.commentedCommentedList(Codec.FLOAT, Comment.constant("test"), builder -> builder.whenIndexIs(1, "bruh")).fieldOf("floatList").forGetter(TestObject::floatList)
                 ).apply(instance, TestObject::new)
         );
     }
