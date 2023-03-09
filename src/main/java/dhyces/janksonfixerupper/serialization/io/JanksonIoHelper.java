253fa9c7a3f6a03e84baf14f39bfe3a724ae7823
dhyces <pokmon987@gmail.com>
2023-03-08 19:53:40 -0500

    Add a write method for the io helper

--- src/main/java/dhyces/janksonfixerupper/serialization/io/JanksonIoHelper.java
+++ src/main/java/dhyces/janksonfixerupper/serialization/io/JanksonIoHelper.java
@@ -2,6 +2,7 @@ package dhyces.janksonfixerupper.serialization.io;
 
 import blue.endless.jankson.Jankson;
 import blue.endless.jankson.JsonElement;
+import blue.endless.jankson.JsonGrammar;
 import blue.endless.jankson.api.SyntaxError;
 import com.google.common.base.Suppliers;
 import com.mojang.serialization.Codec;
@@ -9,11 +10,13 @@ import com.mojang.serialization.DataResult;
 import dhyces.janksonfixerupper.serialization.JanksonOps;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.function.Supplier;
 
 public class JanksonIoHelper {
     public static final Supplier<Jankson> DEFAULT_JANKSON = Suppliers.memoize(() -> Jankson.builder().build());
+    public static final JsonGrammar STANDARD_JSON5 = new JsonGrammar.Builder().bareSpecialNumerics(true).printUnquotedKeys(true).build();
 
     public static DataResult<JsonElement> read(Path path) {
         return read(path, DEFAULT_JANKSON.get());
@@ -37,5 +40,21 @@ public class JanksonIoHelper {
     public static <T> DataResult<T> parseCodec(Path path, Codec<T> codec, Jankson jankson) {
         DataResult<JsonElement> elementDataResult = read(path, jankson);
         return elementDataResult.flatMap(element -> codec.parse(JanksonOps.INSTANCE, element));
+    }
+
+    public static DataResult<JsonElement> write(Path path, DataResult<JsonElement> jsonResult) {
+        return write(path, jsonResult, STANDARD_JSON5);
+    }
+
+    public static DataResult<JsonElement> write(Path path, DataResult<JsonElement> jsonResult, JsonGrammar grammar) {
+        if (jsonResult.error().isPresent()) {
+            return jsonResult;
+        }
+        try {
+            Files.write(path, jsonResult.result().get().toJson(grammar).getBytes());
+        } catch (IOException e) {
+            return jsonResult.mapError(s -> e.getMessage());
+        }
+        return jsonResult;
     }
 }
